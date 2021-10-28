d226dcb6e0f7c5f1b90ab0e81492d7a524b36950
Phạm T. Hiển <55367471+hienpham120@users.noreply.github.com>
2021-10-28 12:01:42 +0700

    'fetchApi'

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+// Components
+import { Grid, Drawer, LinearProgress, Badge } from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons'
+// Styles
+import { Wrapper } from "./App.style"
+import { CartItemType } from './models'
+import { Item } from './components/Item/Item'
 
+
+const getProducts = async (): Promise<CartItemType[]> => await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('prducts', getProducts);
+
+  const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Somthing went wrong...</div>
+
   return (
-    <div className="App">
-      Halo
-    </div>
+    <Wrapper>
+      <Grid container spacing={2}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={6} md={4} lg={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
---
+++ src/components/Item/Item.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Button } from '@material-ui/core'
+// Types
+import { CartItemType } from '../../models'
+// Styles
+import { Wrapper } from './Item.style'
+
+export interface ItemProps {
+    item: CartItemType;
+    handleAddToCart: (clickedItem: CartItemType) => void;
+}
+
+export function Item({ item, handleAddToCart }: ItemProps) {
+    return (
+        <Wrapper>
+            <img src={item.image} alt={item.title} />
+            <div>
+                <h3>{item.title}</h3>
+                <p>{item.description}</p>
+                <h3>{item.price}</h3>
+            </div>
+            <Button variant='contained' onClick={() => handleAddToCart(item)}>Add to cart</Button>
+        </Wrapper>
+    );
+}
